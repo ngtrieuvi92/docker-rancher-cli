3346f15391b18efebf1718b86f55ce14b3d2eeee
vi.nt <vi.nt@geekup.vn>
2016-09-20 16:27:27 +0700

    upgrade entrypoint scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@
 DIR="/data"
 # init
 # look for empty director
-
-EXECUTE_SCRIPT="/data/${RANCHER_EXECUTE_SCRIPT:-run.sh}" 
+$EXEC_FILE=${RANCHER_EXECUTE_SCRIPT:-run.sh}
+EXECUTE_SCRIPT="/data/$EXEC_FILE" 
 if [ "$(ls -A $DIR)" ]; then
     if [ -f "$EXECUTE_SCRIPT" ]; then 
-     echo "start script $EXECUTE_SCRIPT"   
+     echo "Starting script $EXECUTE_SCRIPT"   
      . $EXECUTE_SCRIPT
     else 
      echo "$EXECUTE_SCRIPT is not found!"
